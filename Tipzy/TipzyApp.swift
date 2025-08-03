bbe91ade30d454e3b9e01a45d53930eec1276f7e
Joseph Seoudy <josephakseoudy@gmail.com>
2025-08-02 23:46:24 -0400

    Fixed History and calculate bug

--- Tipzy/TipzyApp.swift
+++ Tipzy/TipzyApp.swift
@@ -2,9 +2,11 @@ import SwiftUI
 
 @main
 struct TipzyApp: App {
+    @StateObject private var historyStore = HistoryStore()
+
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environmentObject(historyStore)
         }
-    }
-}
+    }}
